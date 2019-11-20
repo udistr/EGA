35ea13488ba33b1ba3678ee0eb453c176213150e
udistr <udistr@gmail.com>
2019-11-19 22:28:29 -0500

    Add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='SLA',
+      version='0.1',
+      description='sequential learning algorithms',
+      url='https://github.com/udistr/SLA.git',
+      author='Ehud Strobach',
+      author_email='udistr@gmail.com',
+      license='MIT',
+      packages=['SLA'],
+      zip_safe=False)
